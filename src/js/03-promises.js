eb0838e1242b836040c817324637461e52a4e506
Ellena <106475100+Ellena83@users.noreply.github.com>
2022-10-13 11:51:00 +0200

    update promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,37 +1,3 @@
-// const delayFirst = document.querySelector('input[name=delay]');
-// console.log(delayFirst)
-// const delayStep = document.querySelector('input[name=step]');
-// console.log(delayStep)
-// const amount = document.querySelector('input[name=amount]');
-// console.log(amount)
-
-// const createPromise = (position, delay) => {
-//   return new Promise((resolve, reject) => {
-// setTimeout(() => {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     resolve( {position: position.id, delay: delayFirst.value} );
-//     } 
-//     else {
-//      reject(position);
-//   }
-// }, delay)
-//   })
-// }
-
-// const formRef = document.querySelector('.form');
-// formRef.addEventListener('submit', () => {
-//   createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-// })
-
-
-
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const formRef = document.querySelector('.form');
